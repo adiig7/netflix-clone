fc1640c0d318024fb8242c4bf3be753f49c3b4a6
Aditya Gupta <adutyagupta765469@gmail.com>
2022-02-10 19:55:33 +0530

    navbar component complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import { ArrowDropDown, Notifications, Search } from '@material-ui/icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.scss'
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
+    console.log(isScrolled);
   return (
-      <div className='navbar'>
+      <div className={isScrolled ? "navbar scrolled" : "navbar"}>
           <div className="container">
               
               <div className="left">
@@ -19,18 +25,24 @@ const Navbar = () => {
               </div>
 
               <div className="right">
-                  <Search />
+                  <Search className='icon'/>
                   <span>KIDS</span>
-                  <Notifications />
+                  <Notifications className='icon'/>
                   <img
                       src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                       alt=""
                   />
-                  <ArrowDropDown />
+                  <div className="profile">
+                      <ArrowDropDown className='icon' />
+                      <div className="options">
+                          <span>Settings</span>
+                          <span>Logout</span>
+                      </div>
+                  </div>
               </div>
 
           </div>
-          Navbar</div>
+        </div>
   )
 }
 
