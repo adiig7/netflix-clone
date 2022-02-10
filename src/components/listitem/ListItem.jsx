c985bccf5bcdbd9dce7203720790c62d990ae6a5
Aditya Gupta <adutyagupta765469@gmail.com>
2022-02-11 00:03:01 +0530

    add list

---
+++ src/components/listitem/ListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './listitem.scss'
+const ListItem = () => {
+  return (
+      <div className='listItem'>
+          item
+    </div>
+  )
+}
+
+export default ListItem
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Featured from '../featured/Featured'
+import Navbar from '../components/navbar/Navbar'
+import Featured from '../components/featured/Featured'
 import './home.scss'
+import List from '../components/list/List'
 
 const home = () => {
   return (
       <div className='home'>
       <Navbar />
       <Featured type="series" />
-      abcd
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   )
 }
