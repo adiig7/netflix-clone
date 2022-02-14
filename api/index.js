0f79290da0871abb1d5c082eab09014cc492491b
Aditya Gupta <adutyagupta765469@gmail.com>
2022-02-14 16:55:35 +0530

    add CRUD for users

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 
 dotenv.config({path: './config/config.env'});
 
@@ -10,7 +11,8 @@ connectDB();
 
 app.use(express.json());
 
-app.use("/api/auth", authRoute); 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute); 
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
--- api/verifyToken.js
+++ api/verifyToken.js
@@ -1,3 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const verify = (req, res, next) => { }
+function verify(req, res, next) {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+            if (err)
+                res.status(403).json("Token is not valid!");
+            req.user = user;
+            next();
+        })
+    } else {
+        return res.status(401).json('Not authenticated');
+    }
+}
+ 
+module.exports = verify;
