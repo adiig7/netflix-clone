53f9c4cfca757e8396d2a477c9b3dcc5ddbb2948
Aditya Gupta <adutyagupta765469@gmail.com>
2022-02-19 23:20:58 +0530

    new commit

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -4,15 +4,15 @@ const verify = require('../verifyToken');
 
 //CREATE
 router.post('/', verify, async (req, res) => {
-    if (req.user.admin) {
+    // console.log(req.user.admin);
+    if (req.user.isAdmin) {
         const newMovie = new Movie(req.body);
         try {
             const savedMovie = await newMovie.save();
             res.status(201).json(savedMovie);
         } catch (err) {
             res.status(500).json(err);
-        }
-    } else {
+        }} else {
         res.status(403).json("You are not authorized to perform this action");
     }
 });
@@ -47,7 +47,7 @@ router.delete('/:id', verify, async (req, res) => {
 });
 
 //GET
-router.get("/", verify, async (req, res) => {
+router.get("/find/:id", verify, async (req, res) => {
     try {
         const movie = await Movie.findById(req.params.id);
         res.status(200).json(movie);
@@ -77,5 +77,16 @@ router.get("/random", verify, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//GET ALL
+router.get("/", verify, async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.status(200).json(movies.reverse());
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
